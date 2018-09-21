7522f5d33c9a25ba6300cba994d4bc6802bb9ad2
Chao Gu <guchao.pku@gmail.com>
2018-09-21 01:04:08 -0400

    format pwo_attenuation.py

--- database/pwo_attenuation.py
+++ database/pwo_attenuation.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
-import numpy
+import argparse as ap
+
+import numpy as np
 from scipy import integrate
 
 tolerance = 1e-4
 
 
 def integrand1(y, x, z, attenuation):
-    return numpy.exp(-numpy.sqrt(x**2 + y**2 + z**2) * attenuation) * z / (x**2 + y**2 + z**2)**1.5
+    r2 = x**2 + y**2 + z**2
+    return np.exp(-np.sqrt(r2) * attenuation) * z / r2**1.5
 
 
 def integrand2(y, x, z, attenuation, reflectance):
-    return numpy.exp(-numpy.sqrt(x**2 + y**2 + z**2) * attenuation + numpy.log(reflectance) * (x + y) / 0.0205) * z / (x**2 + y**2 + z**2)**1.5
+    r2 = x**2 + y**2 + z**2
+    return (np.exp(-np.sqrt(r2) * attenuation) * reflectance**
+            ((x + y) / 0.0205) * z / r2**1.5)
 
 
 def attenuation_fwd(depth, attenuation, reflectance):
@@ -23,9 +28,18 @@ def attenuation_fwd(depth, attenuation, reflectance):
         rr = 0
         for i in range(n // 2 + 1):
             j = n - i
-            x_low, x_high = 0.01025 + (i - 1) * 0.0205 if i > 0 else 0, 0.01025 + i * 0.0205
-            y_low, y_high = 0.01025 + (j - 1) * 0.0205 if j > 0 else 0, 0.01025 + j * 0.0205
-            r, _ = integrate.dblquad(integrand1, x_low, x_high, lambda _: y_low, lambda _: y_high, args=(z, attenuation))
+            x_low = 0.01025 + (i - 1) * 0.0205 if i > 0 else 0
+            x_high = 0.01025 + i * 0.0205
+            y_low = 0.01025 + (j - 1) * 0.0205 if j > 0 else 0
+            y_high = 0.01025 + j * 0.0205
+            r, _ = integrate.dblquad(
+                integrand1,
+                x_low,
+                x_high,
+                lambda _: y_low,
+                lambda _: y_high,
+                args=(z, attenuation),
+            )
             rr += r if i * 2 == n else r * 2
         rr = rr * reflectance**n
         result += rr
@@ -33,11 +47,25 @@ def attenuation_fwd(depth, attenuation, reflectance):
             n_save = n
             break
     x_high = 0.01025 + n_save * 0.0205
-    r, _ = integrate.dblquad(integrand2, 0, x_high, lambda _: x_high - _, lambda _: numpy.inf, args=(z, attenuation, reflectance))
+    r, _ = integrate.dblquad(
+        integrand2,
+        0,
+        x_high,
+        lambda _: x_high - _,
+        lambda _: np.inf,
+        args=(z, attenuation, reflectance),
+    )
     result += r
-    r, _ = integrate.dblquad(integrand2, x_high, numpy.inf, lambda _: 0, lambda _: numpy.inf, args=(z, attenuation, reflectance))
+    r, _ = integrate.dblquad(
+        integrand2,
+        x_high,
+        np.inf,
+        lambda _: 0,
+        lambda _: np.inf,
+        args=(z, attenuation, reflectance),
+    )
     result += r
-    return result / numpy.pi  # we only integrated in Quadrant I
+    return result / np.pi  # we only integrated in Quadrant I
 
 
 def attenuation_bwd(depth, attenuation, reflectance):
@@ -49,9 +77,18 @@ def attenuation_bwd(depth, attenuation, reflectance):
         rr = 0
         for i in range(n // 2 + 1):
             j = n - i
-            x_low, x_high = 0.01025 + (i - 1) * 0.0205 if i > 0 else 0, 0.01025 + i * 0.0205
-            y_low, y_high = 0.01025 + (j - 1) * 0.0205 if j > 0 else 0, 0.01025 + j * 0.0205
-            r, _ = integrate.dblquad(integrand1, x_low, x_high, lambda _: y_low, lambda _: y_high, args=(z, attenuation))
+            x_low = 0.01025 + (i - 1) * 0.0205 if i > 0 else 0
+            x_high = 0.01025 + i * 0.0205
+            y_low = 0.01025 + (j - 1) * 0.0205 if j > 0 else 0
+            y_high = 0.01025 + j * 0.0205
+            r, _ = integrate.dblquad(
+                integrand1,
+                x_low,
+                x_high,
+                lambda _: y_low,
+                lambda _: y_high,
+                args=(z, attenuation),
+            )
             rr += r if i * 2 == n else r * 2
         rr = rr * reflectance**(n + 1)
         result += rr
@@ -59,31 +96,44 @@ def attenuation_bwd(depth, attenuation, reflectance):
             n_save = n
             break
     x_high = 0.01025 + n_save * 0.0205
-    r, _ = integrate.dblquad(integrand2, 0, x_high, lambda _: x_high - _, lambda _: numpy.inf, args=(z, attenuation, reflectance))
+    r, _ = integrate.dblquad(
+        integrand2,
+        0,
+        x_high,
+        lambda _: x_high - _,
+        lambda _: np.inf,
+        args=(z, attenuation, reflectance),
+    )
     result += r * reflectance
-    r, _ = integrate.dblquad(integrand2, x_high, numpy.inf, lambda _: 0, lambda _: numpy.inf, args=(z, attenuation, reflectance))
+    r, _ = integrate.dblquad(
+        integrand2,
+        x_high,
+        np.inf,
+        lambda _: 0,
+        lambda _: np.inf,
+        args=(z, attenuation, reflectance),
+    )
     result += r * reflectance
-    return result / numpy.pi  # we only integrated in Quadrant I
+    return result / np.pi  # we only integrated in Quadrant I
 
 
-if __name__ == '__main__':
-    import argparse as ap
+parser = ap.ArgumentParser(prog='pwo_attenuation.py')
+parser.add_argument(
+    'inputs', nargs=2, help='penetration depth (m) and reflectance')
 
-    parser = ap.ArgumentParser(prog='pwo_attenuation.py')
-    parser.add_argument('inputs', nargs=2, help='penetration depth (m) and reflectance')
+args = vars(parser.parse_args())
 
-    args = vars(parser.parse_args())
+att_input = 1 / float(args['inputs'][0])
+ref_input = float(args['inputs'][1])
 
-    att_input = 1 / float(args['inputs'][0])
-    ref_input = float(args['inputs'][1])
+print(att_input, ref_input)
+if ref_input > 1:
+    print('bad input')
+    quit()
 
-    print(att_input, ref_input)
-    if ref_input > 1:
-        print('bad input')
-        quit()
-
-    with open('pwo_attenuation.dat', 'w') as f:
-        for depth in range(180):
-            factor = attenuation_fwd(depth / 1000, att_input, ref_input) + attenuation_bwd(depth / 1000, att_input, ref_input)
-            print(depth, factor)
-            f.write('{}  {}\n'.format(depth, factor))
+with open('pwo_attenuation.dat', 'w') as f:
+    for dep in range(180):
+        factor = attenuation_fwd(dep / 1000, att_input, ref_input)
+        factor += attenuation_bwd(dep / 1000, att_input, ref_input)
+        print(dep, factor)
+        f.write('{}  {}\n'.format(dep, factor))
